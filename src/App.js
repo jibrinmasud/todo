ef2e424769efa3f9d8d721f60bd3358d3d289b10
Jibrin Masud <jibrinmasud494@gmail.com>
2023-11-03 18:33:54 +0100

    todo app using react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import Header from "./components/Header";
+import Form from "./components/Form";
+import Todo from "./components/Todo";
+import {useState} from "react";
 function App() {
+  const [todo, setTodo] = useState('')
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div className="App">
      <Header></Header>
+     <Form todo={todo} 
+     setTodo={setTodo} 
+     todoList ={todoList} 
+     setTodoList={setTodoList}>
+     </Form>
+     <Todo/>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,25 @@
+import styles from '../style.module.css';
+
+const Form = ({todo,setTodo, todoList, setTodoList}) => {
+    const handleChange =(event)=>{
+        setTodo(event.target.value);
+    };
+    const handleSubmit =(event)=>{
+        event.preventDefault();
+        setTodoList([...todoList, todo])
+    };
+    return(
+        <div className={styles.todoform}>
+            <form onSubmit={handleSubmit}>
+                <input 
+                value= {todo}
+                 onChange={handleChange}
+                 className={styles.todoinput} 
+                 placeholder="Add Todo Item">
+                 </input>
+                <button type='submit' className={styles.todobutton}>Add</button>
+            </form>
+        </div>
+    );
+};
+export default Form;
